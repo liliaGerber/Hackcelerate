b16213af7bfdfbf7fe18d2ab41caee60f98f70e0
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2025-03-20 12:01:09 +0100

    linting

--- tests/predict_personality.py
+++ tests/predict_personality.py
@@ -3,29 +3,29 @@ import re
 
 import pandas as pd
 
-cEXT = pickle.load(open("data/models/cEXT.p", "rb"))
-cNEU = pickle.load(open("data/models/cNEU.p", "rb"))
-cAGR = pickle.load(open("data/models/cAGR.p", "rb"))
-cCON = pickle.load(open("data/models/cCON.p", "rb"))
-cOPN = pickle.load(open("data/models/cOPN.p", "rb"))
+C_EXT = pickle.load(open("data/models/cEXT.p", "rb"))
+C_NEU = pickle.load(open("data/models/cNEU.p", "rb"))
+C_AGR = pickle.load(open("data/models/cAGR.p", "rb"))
+C_CON = pickle.load(open("data/models/cCON.p", "rb"))
+C_OPN = pickle.load(open("data/models/cOPN.p", "rb"))
 vectorizer_31 = pickle.load(open("data/models/vectorizer_31.p", "rb"))
 vectorizer_30 = pickle.load(open("data/models/vectorizer_30.p", "rb"))
 
 
 def predict_personality(text):
-    scentences = re.split("(?<=[.!?]) +", text)
-    text_vector_31 = vectorizer_31.transform(scentences)
-    text_vector_30 = vectorizer_30.transform(scentences)
-    EXT = cEXT.predict(text_vector_31)
-    NEU = cNEU.predict(text_vector_30)
-    AGR = cAGR.predict(text_vector_31)
-    CON = cCON.predict(text_vector_31)
-    OPN = cOPN.predict(text_vector_31)
-    return [EXT[0], NEU[0], AGR[0], CON[0], OPN[0]]
+    sentences = re.split("(?<=[.!?]) +", text)
+    text_vector_31 = vectorizer_31.transform(sentences)
+    text_vector_30 = vectorizer_30.transform(sentences)
+    ext = C_EXT.predict(text_vector_31)
+    neu = C_NEU.predict(text_vector_30)
+    agr = C_AGR.predict(text_vector_31)
+    con = C_CON.predict(text_vector_31)
+    opn = C_OPN.predict(text_vector_31)
+    return [ext[0], neu[0], agr[0], con[0], opn[0]]
 
 
 text = "It is important to note that each of the five personality "
 
 predictions = predict_personality(text)
 print("predicted personality:", predictions)
-df = pd.DataFrame(dict(r=predictions, theta=["EXT", "NEU", "AGR", "CON", "OPN"]))
+df = pd.DataFrame({"r": predictions, "theta": ["EXT", "NEU", "AGR", "CON", "OPN"]})
